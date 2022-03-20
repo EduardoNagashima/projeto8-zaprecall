8ee710abace8972a130680dc54e6ea0053013c2f
Eduardo Nagashima <eduyudji@gmail.com>
2022-03-20 10:51:44 -0300

    hotfix

--- src/components/Tela2/Tela2.jsx
+++ src/components/Tela2/Tela2.jsx
@@ -12,14 +12,6 @@ export default function Tela2() {
   const [counter, setCounter] = useState(0);
   const [sequencia, setSequencia] = useState([]);
 
-
-  function a() {
-    Live(work => <Viver jogar="{true}">{work}</Viver>);
-  }
-
-
-
-
   return (
     <div className="tela2">
 
